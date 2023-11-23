2b038947c63aae001077004d1af0a01355a52a7f
Olly Evans <136471380+olly-evans@users.noreply.github.com>
2023-11-23 01:01:18 +0000

    start model creation

--- traffic.py
+++ traffic.py
@@ -96,7 +96,22 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+
+    # cnn
+    model = tf.keras.models.Sequential(
+        # Convolutional layer. 32 filters using 3x3 kernel.
+        tf.keras.layers.Conv2D(32, (3,3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+
+        # Max-pooling layer, using 2x2 pool size.
+        tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+
+        # Flatten units.
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout.
+    )
+
+    
 
 
 if __name__ == "__main__":
